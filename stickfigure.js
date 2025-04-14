3f447d11e00cf45302569db2f2653ff6549889e4
atofus <alantofus@gmail.com>
2025-04-13 18:06:17 -0700

    Added more features. Also fixed animation problem.

--- stickfigure.js
+++ stickfigure.js
@@ -2,7 +2,7 @@ class Figure {
     constructor(game, x, y) {
         Object.assign(this, {game, x, y});
         
-        this.state = 0; //0 = running, 1 = jumping, 2 = falling, 3 = sliding
+        this.state = 3; //0 = running, 1 = jumping, 2 = falling, 3 = sliding
         this.facing = 0; //0 = left, 1 = right
         this.light = 0; //0 = light mode, 1 =  dark-mode
         
@@ -12,9 +12,9 @@ class Figure {
 
 
 
-        this.x = 0;
-        this.y = 20;
-        this.speed = 220;
+        this.x = -250;
+        this.y = 78;
+        this.speed = 260;
 
         this.loadAnimations();
     }
@@ -32,16 +32,16 @@ class Figure {
         }
         
         //light mode facing right
-        this.animations[0][0][0] = new Animator(ASSET_MANAGER.getAsset("./images/run-white.png"), 67, 27, 201, 208, 5, 0.08, false, true);
-        this.animations[0][1][0] = new Animator(ASSET_MANAGER.getAsset("./images/jump-white.png"), 67, 27, 201, 208, 5, 0.08, false, true);
-        this.animations[0][2][0] = new Animator(ASSET_MANAGER.getAsset("./images/fall-white.png"), 67, 27, 201, 208, 5, 0.08, false, true);
-        this.animations[0][3][0] = new Animator(ASSET_MANAGER.getAsset("./images/sliding-white.png"), 67, 27, 201, 208, 5, 0.08, false, true);
+        this.animations[0][0][0] = new Animator(ASSET_MANAGER.getAsset("./images/run-white.png"), 67, 27, 201, 208, 6, 0.08, false, true);
+        this.animations[0][1][0] = new Animator(ASSET_MANAGER.getAsset("./images/jump-white.png"), 0, 30, 188, 208, 1, 1, false, true);
+        this.animations[0][2][0] = new Animator(ASSET_MANAGER.getAsset("./images/fall-white.png"), -25, 20, 170, 175, 3, 0.3, false, true);
+        this.animations[0][3][0] = new Animator(ASSET_MANAGER.getAsset("./images/slide-white.png"), 17, 30, 204, 175, 3, 0.6, false, true);
 
         //light mode facing left
-        this.animations[1][0][0] = new Animator(ASSET_MANAGER.getAsset("./images/run-white-flipped.png"), 67, 27, 201, 208, 5, 0.08, true, true);
-        this.animations[1][1][0] = new Animator(ASSET_MANAGER.getAsset("./images/jump-white-flipped.png"), 67, 27, 201, 208, 5, 0.08, true, true);
-        this.animations[1][2][0] = new Animator(ASSET_MANAGER.getAsset("./images/fall-white-flipped.png"), 67, 27, 201, 208, 5, 0.08, true, true);
-        this.animations[1][3][0] = new Animator(ASSET_MANAGER.getAsset("./images/sliding-white-flipped.png"), 67, 27, 201, 208, 5, 0.08, true, true);
+        this.animations[1][0][0] = new Animator(ASSET_MANAGER.getAsset("./images/run-white-flipped.png"), -7, 27, 201, 208, 5, 0.08, true, true);
+        this.animations[1][1][0] = new Animator(ASSET_MANAGER.getAsset("./images/jump-white-flipped.png"), 180, 30, 188, 208, 1, 1, true, true); //work on left features
+        this.animations[1][2][0] = new Animator(ASSET_MANAGER.getAsset("./images/fall-white-flipped.png"),-25, 20, 170, 175, 3, 0.3, true, true);
+        this.animations[1][3][0] = new Animator(ASSET_MANAGER.getAsset("./images/slide-white-flipped.png"), -7, 27, 201, 208, 5, 0.08, true, true);
 
         
 
@@ -51,7 +51,7 @@ class Figure {
         this.x += this.speed * this.game.clockTick;
         //if they go across the screen, just set it back to beginning!
         if (this.x > 1300) {
-            this.x = 0;
+            this.x = -250;
         }
         console.log("ello mate");
     }
